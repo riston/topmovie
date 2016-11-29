565cdae8e705e10dadbae8e51cc8160a5b9efbeb
riston <est.risto@gmail.com>
2016-11-29 20:52:56 +0200

    Wrong project name in config

--- config/prod.exs
+++ config/prod.exs
@@ -59,7 +59,7 @@ config :logger, level: :info
 #
 
 # Configure your database
-config :hello_phoenix, HelloPhoenix.Repo,
+config :topmovie, Topmovie.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
